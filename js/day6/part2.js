8572a702aa9a760e93d661a089635cb1a27c3303
Willian Martins <2333508+wmsbill@users.noreply.github.com>
2023-12-06 18:45:28 -0800

    day6 with math

--- js/day6/part2.js
+++ js/day6/part2.js
@@ -10,20 +10,17 @@ function parse(fileContent) {
   return [time, distance];
 }
 
+function calculateWins(time, record) {
+  const memo = Math.sqrt(time * time - 4 * record);
+  const lowerBound = Math.floor(((-time + memo) / 2) * -1) + 1;
+  const upperBound = Math.ceil(((-time - memo) / 2) * -1) - 1;
+  return upperBound - lowerBound + 1;
+}
+
 function main(file) {
   const fileContent = fs.readFileSync(file, "utf8");
   const [time, record] = parse(fileContent);
-  let wins = 0;
-
-  for (let i = 0; i < time; i++) {
-    const distance = (time - i) * i;
-
-    if (distance > record) {
-      wins++;
-    }
-  }
-
-  return wins;
+  return calculateWins(time, record);
 }
 
 const result = main(args[0] ?? "./input/day6/1.test.txt");
